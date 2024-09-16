e6b2b9fdc2e7b5fa2badb9d77c81543afcd08163
Leo jaime <anderson@gmail.com>
2024-09-16 12:45:36 -0300

    Renomeado nome da função

---
+++ constants/Options.js
@@ -0,0 +1,30 @@
+export const SelectTravelerList=[
+    {
+        id:1,
+        title:'Apenas eu',
+        desc:'Viajando e explorando sozinho',
+        icon:'🧙‍♂️',
+        people:'1 pessoa'
+    },
+    {
+        id:2,
+        title:'Um casal',
+        desc:'Duas pessoas viajando em conjunto',
+        icon:'👫',
+        people:'2 pessoas'
+    },
+    {
+        id:3,
+        title:'Familia',
+        desc:'Viajando e  curtindo em familia',
+        icon:'👨‍👩‍👦‍👦',
+        people:'3 a 5 pessoas'
+    },
+    {
+        id:4,
+        title:'Amigos',
+        desc:'Um bando de caçadores de emoções',
+        icon:'🥷🥷',
+        people:'5 a 10 pessoas'
+    },
+]
