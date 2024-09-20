6d2dfec09b49a3d9c7c5bec057ea35422a114918
Leo jaime <anderson@gmail.com>
2024-09-20 00:46:34 -0300

    Imprementado funcao GenerateAirTrip que pega as infomacoes do cliente e monta o prompt para API do GEMINI

--- app/create-trip/generate-trip.jsx
+++ app/create-trip/generate-trip.jsx
@@ -1,13 +1,49 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {Colors} from './../../constants/Colors';
+import { CreateTripContext } from '../../context/CreateTripContext';
+import { AI_PROMPT } from '../../constants/Options';
+import { chatSession } from '../../configs/AiModal';
+import { useRouter } from 'expo-router';
+
+
 export default function GenerateTrip() {
+
+  const {tripData,setTripData}=useContext(CreateTripContext);
+  const [loading,setLoading]=useState(false);
+  const router=useRouter();
+  useEffect(() => {
+    tripData&&GenerateAirTrip()
+  },[tripData]);
+
+  const GenerateAirTrip=async()=>{
+    setLoading(true);
+    const FINAL_PROMPT=AI_PROMPT
+    .replace('{location}',tripData?.locationInfo?.name)
+    .replace('{totalDays}',tripData?.totalNoOfDays)
+    .replace('{totalNight}',tripData?.totalNoOfDays-1)
+    .replace('{traveler}',tripData?.travelerCount?.title)
+    .replace('{budget}',tripData?.budget?.title)
+    .replace('{totalDays}',tripData?.totalNoOfDays)
+    .replace('{totalNight}',tripData?.totalNoOfDays-1)
+
+    console.log(FINAL_PROMPT);
+
+    // Verificar respota do gemini
+    // const result = await chatSession.sendMessage(FINAL_PROMPT);
+    // console.log(result.response.text());
+    setLoading(false)
+    
+    router.push('(tabs)/mytrip');
+  }
+
   return (
     <View style={{
         padding:25,
         marginTop:75,
         backgroundColor:Colors.WHITE,
-        height:'100%'
+        height:'100%',
+        alignItems: 'center'
     }}>
       <Text style={{
         fontFamily:'outfit-bold',
@@ -22,12 +58,29 @@ export default function GenerateTrip() {
         textAlign: 'center',
         marginTop:40,
       }}>
-        Estamos preparando a viagem do seus sonhos
+        Estamos preparando a viagem dos seus sonhos
       </Text>
       <Image  source={require('./../../assets/images/tela-carregamento.gif')}
       style={{
         alignContent: 'center',
+        objectFit: 'center',
       }} />
+      <Text style={{
+        fontFamily:'outfit',
+        color:Colors.GRAY,
+        fontSize:20,
+        textAlign:'Center',
+
+      }}>Os dados estão viajando rápido!</Text>
+
+      <Text style={{
+        fontFamily:'outfit',
+        color:Colors.GRAY,
+        fontSize:20,
+        textAlign:'Center',
+
+      }}>Aguarde a chegada.</Text>
+
     </View>
   )
 }
